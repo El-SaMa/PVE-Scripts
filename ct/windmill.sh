69d65cdf3af9d04ea58c32d84bbb0267aea25193
El Sayed Mahmoud <113467305+El-SaMa@users.noreply.github.com>
2025-04-18 22:21:09 +0300

    Update windmill.sh

--- ct/windmill.sh
+++ ct/windmill.sh
@@ -1,40 +1,29 @@
 #!/usr/bin/env bash
-
-# Windmill.dev Proxmox LXC Installer
-# Author: elsama
-# Inspired by tteck
+source <(curl -s https://raw.githubusercontent.com/community-scripts/ProxmoxVE/main/misc/build.func)
 APP="Windmill.dev"
-CTID=902
-TEMPLATE="local:vztmpl/debian-12-standard_20240311_amd64.tar.zst"
-HOSTNAME="windmill"
-DISK_SIZE="8"
-CPU="2"
-RAM="2048"
-BRIDGE="vmbr0"
-UNPRIV="1"
+var_tags="devops;automation;orchestration"
+var_cpu="4"
+var_ram="2048"
+var_disk="8"
+var_os="debian"
+var_version="12"
+var_unprivileged="1"
 
-echo -e "\033[1;92mCreating LXC container for $APP...\033[0m"
+header_info "$APP"
+variables
+color
+catch_errors
 
-pct create $CTID $TEMPLATE \
-  --rootfs local-lvm:${DISK_SIZE}G \
-  --hostname $HOSTNAME \
-  --net0 name=eth0,bridge=$BRIDGE,ip=dhcp \
-  --cores $CPU \
-  --memory $RAM \
-  --unprivileged $UNPRIV \
-  --features nesting=1,keyctl=1 \
-  --start 1 \
-  --onboot 1
+function install_script() {
+  bash -c "$(curl -fsSL https://raw.githubusercontent.com/El-SaMa/PVE-Scripts/main/install/windmill-install.sh)"
+}
 
-echo -e "\033[1;92m✓ LXC $CTID created and started. Installing $APP...\033[0m"
-
-pct exec $CTID -- bash -c "
-  apt update &&
-  apt install -y curl git docker.io docker-compose &&
-  git clone https://github.com/windmill-labs/windmill.git /opt/windmill &&
-  cd /opt/windmill &&
-  cp .env.sample .env &&
-  docker-compose up -d --build
-"
-
-echo -e "\n\033[1;92m✓ All done. Check Windmill at: http://<container-IP>:3000\033[0m"
+start
+build_container
+description
+msg_ok "Completed Successfully!\n"
+echo -e "${CREATING}${GN}${APP} setup has been successfully initialized!${CL}"
+echo -e "${INFO}${YW} Attach to the container with:${CL}"
+echo -e "${TAB}${BL}lxc-attach -n ${CTID}${CL}"
+echo -e "${INFO}${YW} Then run the install script manually if needed:${CL}"
+echo -e "${TAB}${BL}install_script${CL}"
